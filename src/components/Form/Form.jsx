22c20c5726b94777a9754d77e0eab2fb2c39eed0
mylesd <mylesd@sfu.ca>
2018-08-02 16:41:26 -0700

    Added basic form validation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import './form.css'
+import './Form.css'
 
 export default class Form extends React.Component {
   constructor(props) {
@@ -9,8 +9,14 @@ export default class Form extends React.Component {
 
     this.state = {
       title: '',
+      titleError: false,
+      titleErrorMsg: '',
       description: '',
-      image: ''
+      descriptionError: false,
+      descriptionErrorMsg: '',
+      image: '',
+      imageError: false,
+      imageErrorMsg: '',
     }
   }
 
@@ -20,8 +26,58 @@ export default class Form extends React.Component {
     });
   }
 
+  fieldIsEmpty = (str) => {
+    if(!str) return true;
+    return false;
+  }
+
+  fieldsAreValid = (title, description, image) => {
+    this.setState({
+      titleError: false,
+      titleErrorMsg: '',
+      descriptionError: false,
+      descriptionErrorMsg: '',
+      imageError: false,
+      imageErrorMsg: '',
+    });
+
+    let errors = {};
+    let errorMessages = {};
+    if(fieldIsEmpty(title)) {
+      errorMessages.titleErrorMsg = 'No title set';
+      errors.titleError = true;
+    }
+    if(fieldIsEmpty(description)) {
+      errorMessages.descriptionErrorMsg = 'No description set';
+      errors.descriptionError = true;
+    }
+    if(fieldIsEmpty(image)) {
+      errorMessages.imageErrorMsg = 'No image set';
+      errors.imageError = true;
+    }
+
+    this.setState({
+      ...errors,
+      ...errorMessages,
+    })
+
+    if(Object.keys(errors).length === 0){
+      this.setState({
+        title: '',
+        description: '',
+        image: '',
+      })
+      return true;
+    }
+    return false;
+  }
+
   handleSubmit = (e) => {
     const { title, description, image } = this.state;
+    
+    if(!this.fieldsAreValid(title, description, image)) {
+      return;
+    }
     this.props.handleSubmit(title, description)
   }
 
@@ -30,28 +86,34 @@ export default class Form extends React.Component {
     return (
       <form className='flex-container'>
         <TextField
+          error={this.state.titleError}
           name='title'
           label='Title'
           value={this.state.title}
           onChange={this.handleChange}
           className='text-field'
-          margin="normal"
+          margin="dense"
+          helperText={this.state.titleErrorMsg}
         />
         <TextField
+          error={this.state.descriptionError}
           name='description'
           label='Description'
           value={this.state.description}
           onChange={this.handleChange}
           className='text-field'
-          margin="normal"
+          margin="dense"
+          helperText={this.state.descriptionErrorMsg}
         />
         <TextField
+          error={this.state.imageError}
           name='image'
           label='Image Url'
           value={this.state.image}
           onChange={this.handleChange}
           className='text-field'
-          margin="normal"
+          margin="dense"
+          helperText={this.state.imageErrorMsg}
         />
         <Button
           variant='contained'
